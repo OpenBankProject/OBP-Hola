1168b5214bf1f627bf2b1bda3f8f18a4ce478b3e
shuang <shuang@tesobe.com>
2020-10-12 08:29:50 +0800

    disable ObpHydraAuthApplicationTests

--- src/test/java/com/openbankproject/hydra/auth/ObpHydraAuthApplicationTests.java
+++ src/test/java/com/openbankproject/hydra/auth/ObpHydraAuthApplicationTests.java
@@ -1,5 +1,6 @@
 package com.openbankproject.hydra.auth;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +9,9 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
-//@SpringBootTest
+@SpringBootTest
 @ActiveProfiles("local")
+@Disabled
 class ObpHydraAuthApplicationTests {
     @Resource
     private RestTemplate restTemplate;
@@ -19,7 +21,7 @@ class ObpHydraAuthApplicationTests {
         ResponseEntity<String> forEntity = restTemplate.getForEntity("https://api-mtls.ofpilot.com/obp/v4.0.0/banks", String.class);
         ResponseEntity<String> forEntity2 = restTemplate.getForEntity("https://oauth2.api-mtls.ofpilot.com/hydra-public/.well-known/openid-configuration", String.class);
 //        ResponseEntity<String> forEntity = restTemplate.getForEntity("https://api-mtls.ofpilot.com/mx-open-finance/v0.0.1/accounts", String.class);
-        System.out.println();
+        System.out.println("------------------");
     }
 
 }
