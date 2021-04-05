213ea0b6f92c09b846f7a3461ae1a37cd49f510a
Marko Milić <marko@tesobe.com>
2021-04-05 21:09:30 +0200

    feature/Add signing request; Add request headers: digest, x-jws-signature

--- src/main/java/com/openbankproject/hydra/auth/ClientConfig.java
+++ src/main/java/com/openbankproject/hydra/auth/ClientConfig.java
@@ -1,45 +1,18 @@
-package com.openbankproject;
+package com.openbankproject.hydra.auth;
 
 import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.JWSSigner;
-import com.nimbusds.jose.crypto.ECDSASigner;
-import com.nimbusds.jose.crypto.RSASSASigner;
-import com.nimbusds.jose.crypto.bc.BouncyCastleProviderSingleton;
-import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.SignedJWT;
-import com.openbankproject.hydra.auth.VO.WellKnown;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.Security;
+import java.security.*;
 import java.security.cert.CertificateException;
 import java.text.ParseException;
-import java.time.Instant;
-import java.util.Date;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Stream;
 
 @Configuration
 public class ClientConfig {
@@ -53,16 +26,31 @@ public class ClientConfig {
 
     @PostConstruct
     private void initiate() throws ParseException, JOSEException {
-        // Load the key store from file
+/*        // Load the key store from file
         try {
             // Specify the key store type, e.g. JKS
             KeyStore keyStore = KeyStore.getInstance("JKS");
             // If you need a password to unlock the key store
-            char[] password = "changeit".toCharArray();
-            keyStore.load(new FileInputStream("/home/marko/Tesobe/GitHub/OBP-Hydra-OAuth2/src/main/resources/cert/ofpilot.jks"), password);
+            char[] password = "waiJaeP7sake".toCharArray();
+            keyStore.load(new FileInputStream("/home/marko/Tesobe/GitHub/OBP-Hydra-OAuth2/src/main/resources/cert/user.jks"), password);
             // Extract keys and output into JWK set; the secord parameter allows lookup 
             // of passwords for individual private and secret keys in the store
             JWKSet jwkSet = JWKSet.load(keyStore, null);
+
+
+            String alias = "myalias";
+
+            Key key = null;
+            try {
+                key = keyStore.getKey(alias, "waiJaeP7sake".toCharArray());
+            } catch (UnrecoverableKeyException e) {
+                e.printStackTrace();
+            }
+            if (key instanceof PrivateKey) {
+                key = (PrivateKey) key;
+            }
+            
+            
             jwkClient = jwkSet.getKeys().get(0);
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,7 +58,7 @@ public class ClientConfig {
             e.printStackTrace();
         } catch (CertificateException | KeyStoreException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
 }
--- src/main/java/com/openbankproject/hydra/auth/RestTemplateConfig.java
+++ src/main/java/com/openbankproject/hydra/auth/RestTemplateConfig.java
@@ -1,7 +1,12 @@
 package com.openbankproject.hydra.auth;
 
+import com.nimbusds.jose.jwk.RSAKey;
+import com.openbankproject.JwsUtil;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpRequest;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClients;
@@ -16,11 +21,17 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.*;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.GeneralSecurityException;
-import java.security.KeyStore;
-import java.security.SecureRandom;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class RestTemplateConfig {
@@ -33,6 +44,11 @@ public class RestTemplateConfig {
     private Resource trustStoreResource;
     @Value("${mtls.trustStore.password}")
     private char[] trustStorePassword;
+
+    @javax.annotation.Resource
+    private ClientConfig clientConfig;
+    @javax.annotation.Resource
+    private HydraConfig hydraConfig;
 
     @Bean
     public RestTemplate restTemplate(SSLContext sslContext) {
@@ -84,5 +100,91 @@ public class RestTemplateConfig {
         if(ArrayUtils.isEmpty(headers)) {
             request.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
         }
+        String url = request.getRequestLine().getUri();
+        String httpMethod = request.getRequestLine().getMethod().toLowerCase();
+        String httpBody = getHttpBody(request).toString();
+        InetAddress ip = getInetAddress();
+        JwsUtil jwsUtil = new JwsUtil();
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("host", request.getFirstHeader("host").getValue());
+        requestHeaders.put("content-type", request.getFirstHeader("content-type").getValue());
+        requestHeaders.put("psu-ip-address", ip.getHostAddress());
+        request.setHeader("psu-ip-address", ip.getHostAddress());
+        requestHeaders.put("psu-geo-location", "GEO:52.506931,13.144558");
+        request.setHeader("psu-geo-location", "GEO:52.506931,13.144558");
+        String digest = jwsUtil.createDigestHeaderValue(httpBody.toString());
+        String xJwsSignature = null;
+        request.setHeader("digest", digest);
+        xJwsSignature = JwsUtil.createJwsSignature(getRsaKey(), httpMethod, url, requestHeaders, httpBody.toString());
+        request.setHeader("x-jws-signature", xJwsSignature);
+    }
+
+    private InetAddress getInetAddress() {
+        InetAddress ip = null;
+        try {
+            ip = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return ip;
+    }
+
+    private StringBuilder getHttpBody(HttpRequest request) {
+        StringBuilder httpBody = new StringBuilder();
+        if(request instanceof HttpEntityEnclosingRequest) {
+            HttpEntityEnclosingRequest enclosingRequest = ((HttpEntityEnclosingRequest) request);
+            HttpEntity requestEntity = enclosingRequest.getEntity();
+
+            try {
+                InputStream inputStream =  requestEntity.getContent();
+                try (Reader reader = new BufferedReader(new InputStreamReader
+                        (inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
+                    int c = 0;
+                    while ((c = reader.read()) != -1) {
+                        httpBody.append((char) c);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return httpBody;
+    }
+
+    private RSAKey getRsaKey() {
+        KeyStore ks = null;
+        String alias = "1";
+        Key key = null;
+        try {
+            ks = KeyStore.getInstance("jks");
+            InputStream inputStream = keyStoreResource.getInputStream();
+            ks.load(inputStream, keyStorePassword);
+            key = ks.getKey(alias, keyStorePassword);
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | UnrecoverableKeyException | CertificateException e) {
+            e.printStackTrace();
+        }
+        KeyPair keyPair = null;
+        RSAKey jwk = null;
+        if (key instanceof PrivateKey) {
+            // Get certificate of public key
+            Certificate cert = null;
+            try {
+                cert = ks.getCertificate(alias);
+            } catch (KeyStoreException e) {
+                e.printStackTrace();
+            }
+
+            // Get public key
+            PublicKey publicKey = cert.getPublicKey();
+
+            // Return a key pair
+            keyPair = new KeyPair(publicKey, (PrivateKey) key);
+            // Convert to JWK format
+            jwk = new RSAKey.Builder(
+                    (RSAPublicKey) keyPair.getPublic())
+                    .privateKey(keyPair.getPrivate())
+                    .build();
+        }
+        return jwk;
     }
 }
