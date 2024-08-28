8f9471cf315777f038b7ba585422e205c57233ee
Marko Milić <marko@tesobe.com>
2024-08-28 15:55:20 +0200

    feature/Add Bank Routing to VRP Consents Flow

---
+++ src/main/resources/static/js/index-obp-vrp-html-common-code.js
@@ -0,0 +1,25 @@
+function setBankSelection() {
+    let bank = document.querySelector("#bank");
+    localStorage.setItem('last-bank-selection-obp-vrp-flow', bank.value);
+}
+
+function restoreLastBankSelection() {
+    if(localStorage.getItem('last-bank-selection-obp-vrp-flow')) {
+        let bank = document.querySelector("#bank");
+        bank.value = localStorage.getItem('last-bank-selection-obp-vrp-flow');
+    }
+};
+
+function setBankInAccordanceTo(bankId) {
+    let bank = document.querySelector("#bank");
+    let bankScheme = document.querySelector("#from_bank_routing_scheme");
+    if(bankScheme.value == "OBPBankId") {
+        bank.value = bankId.value;
+    }
+};
+
+window.addEventListener("load", (event) => {
+  console.log("Page fully loaded");
+  // The function to be executed
+  restoreLastBankSelection();
+});
